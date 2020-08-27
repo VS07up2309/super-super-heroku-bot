05a83482acdc13616b960a20b30bdaa2ea51de52
ImCoolAndYouUwU <65310408+VS07up2309@users.noreply.github.com>
2020-08-28 00:10:52 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,12 +5,5 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	
-});
-
-
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.DISCORD_SECRET);

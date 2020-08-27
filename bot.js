a5b47a1e7d668007ff559bb1222393fa7541a5f0
ImCoolAndYouUwU <65310408+VS07up2309@users.noreply.github.com>
2020-08-27 23:58:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ client.on('message', message => {
 
 client.on('guildMemberAdd', member => {
   // Send the message to a designated channel on a server:
-  const channel = member.guild.channels.cache.find(ch => ch.name === 'Welcome');
+  const channel = member.guild.channels.cache.find(ch => ch.name === 'welcome');
   // Do nothing if the channel wasn't found on this server
   if (!channel) return;
   // Send the message, mentioning the member

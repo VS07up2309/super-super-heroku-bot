8f31b333639b40092044483a86d1863663593ed6
ImCoolAndYouUwU <65310408+VS07up2309@users.noreply.github.com>
2020-08-28 21:43:40 +0200

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -3,12 +3,8 @@ module.exports = {
 	description: 'Tag a member and kick them (but not really).',
 	guildOnly: true,
 	execute(message) {
-		if (!message.mentions.users.size) {
-			return message.reply('you need to tag a user in order to kick them!');
-		}
-
 		const taggedUser = message.mentions.users.first();
 
-		message.channel.send(`You wanted to kick: ${taggedUser.username}`);
+		taggedUser.kick();
 	},
 };

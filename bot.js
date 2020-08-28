86440270dc18a7cff2f44d6dd34e5ce53e105cce
ImCoolAndYouUwU <65310408+VS07up2309@users.noreply.github.com>
2020-08-28 20:37:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('
 
 
 
-let spamCtrl = require('./addons/spamCtrl.js');
+
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
@@ -30,19 +30,6 @@ client.on('ready', () => {
 client.user.setActivity(`Serving ${client.guilds.cache.size} servers`);
 
 });
-
-client.on("guildCreate", guild => {
-  // This event triggers when the bot joins a guild.
-  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
-});
-
-client.on("guildDelete", guild => {
-  // this event triggers when the bot is removed from a guild.
-  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
-});
-
 
 client.on('message', message => {
 	if (!message.content.startsWith(config.prefix) || message.author.bot) return;
@@ -67,12 +54,7 @@ client.on('message', message => {
 		}
 
 		return message.channel.send(reply);
-	}
-
-	
-
-
-	
+	}	
 try {
 	command.execute(message, args);
 } catch (error) {
@@ -80,60 +62,6 @@ try {
 	message.reply('there was an error trying to execute that command!');
 }
 });
-
-
- client.on('message', message => {
-if (message.content === '!spam') {  
- spamCtrl.setChannel(message.channel);
-    spamCtrl.setStatus(true);
-}
-});
-
-client.on('message', message => {
-if (message.content === '!ss') {
-    spamCtrl.setStatus(false);
-    
-}
-});
-
- 
-
-
-client.on("message", (message) => {
-     if (message.content.startsWith("!kick")) {
-         // Easy way to get member object though mentions.
-         var member= message.mentions.members.first();
-
-         member.kick().then((member) => {
-             // Successmessage
-             message.channel.send(":wave: " + member.displayName + " has been successfully kicked :D ");
- console.log('KICKED');
-         }).catch(() => {
-              // Failmessage
-             message.channel.send("Access Denied");
-         });
-     }
- });
-
-client.on("message", (message) => {
-    if (message.content.startsWith("!ban")) {
-        // Easy way to get member object though mentions.
-        var member= message.mentions.members.first();
-        // ban
-        member.ban().then((member) => {
-            // Successmessage
-            message.channel.send(":wave: " + member.displayName + " has been successfully banned :D ");
-console.log('BANNED');
-        }).catch(() => {
-             // Failmessage
-            message.channel.send("Access Denied");
-        });
-    }
-});
-
-
-
-  
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.DISCORD_SECRET);

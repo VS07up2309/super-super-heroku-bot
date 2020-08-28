eeaebd2553c3293a59b73d14947305128e872ef0
ImCoolAndYouUwU <65310408+VS07up2309@users.noreply.github.com>
2020-08-28 17:35:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const Discord = require('discord.js');
 
-const config = require('./config.js');
+var config = require('./config.js');
 
 const client = new Discord.Client();
 

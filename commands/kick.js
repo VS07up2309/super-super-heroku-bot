4ff62fb306d3c3465b3e1de5899631429a54bbc7
ImCoolAndYouUwU <65310408+VS07up2309@users.noreply.github.com>
2020-08-28 22:20:29 +0200

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -3,19 +3,17 @@ module.exports = {
 	cooldown: 10,
 	description: 'Kick someone',
 	execute(message) {
+		// Easy way to get member object though mentions.
+	var member= message.mentions.members.first();
+		//kickt den benutzer
+		member.kick().then((member) => {
+			//Success
+			message.channel.send(member.displayName + " wurde gekickt");
+		}).catch(() => {
+			//Fail
+			message.channel.send("Keine Berechtigung");
+		});
+			
 		
-    
-        // Easy way to get member object though mentions.
-        var member= message.mentions.members.first();
-        // Kick
-        member.kick().then((member) => {
-            // Successmessage
-            message.channel.send(":wave: " + member.displayName + " wurde erfolgreich gekickt ");
-        }).catch(() => {
-             // Failmessage
-            message.channel.send("Keine Rechte");
-        });
-    
-};
-	},
-};
+		
+  

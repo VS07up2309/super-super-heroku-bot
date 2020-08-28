d27ccccc30b50a8b262d52c5929f77ff6a144904
ImCoolAndYouUwU <65310408+VS07up2309@users.noreply.github.com>
2020-08-28 17:18:52 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -40,7 +40,7 @@ client.on("guildDelete", guild => {
 
 
 client.on('message', message => {
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
+	if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const command = args.shift().toLowerCase();

f6e5b3663278ed6410d787ccb986e1ce4407b5f3
ImCoolAndYouUwU <65310408+VS07up2309@users.noreply.github.com>
2020-08-28 20:19:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-const cooldowns = new Discord.Collection();
+
 
 let spamCtrl = require('./addons/spamCtrl.js');
 
@@ -69,26 +69,9 @@ client.on('message', message => {
 		return message.channel.send(reply);
 	}
 
-	if (!cooldowns.has(command.name)) {
-	cooldowns.set(command.name, new Discord.Collection());
-}
+	
 
-const now = Date.now();
-const timestamps = cooldowns.get(command.name);
-const cooldownAmount = (command.cooldown || 3) * 1000;
 
-if (timestamps.has(message.author.id)) {
-	// ...
-	const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-
-	if (now < expirationTime) {
-		const timeLeft = (expirationTime - now) / 1000;
-		return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
-		timestamps.set(message.author.id, now);
-setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
-	}
-}
-}
 	
 try {
 	command.execute(message, args);

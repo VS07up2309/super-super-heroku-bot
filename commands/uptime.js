b8d768464efaf703e1d0ad184153a32f3f567b6b
ImCoolAndYouUwU <65310408+VS07up2309@users.noreply.github.com>
2020-08-29 15:22:36 +0200

    Delete uptime.js

--- commands/uptime.js
+++
@@ -1,9 +0,0 @@
-module.export = { 
-  name: 'uptime',
-  execute(message) {
-  const moment = require("moment");
-require("moment-duration-format");
-const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
-message.channel.send(duration);
-}
-};

d062414f0c363ff22bd23b222e2d207c830fb024
ImCoolAndYouUwU <65310408+VS07up2309@users.noreply.github.com>
2020-08-29 14:27:35 +0200

    Update stp.js

--- commands/stp.js
+++ commands/stp.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'stp',
 	description: 'Get the avatar URL of the tagged user(s), or your own avatar.',
 	execute(message, args) {
-const acceptedReplies = ['rock', 'paper', 'scissors'];
+const acceptedReplies = ['stein', 'papier', 'schere'];
         const random = Math.floor((Math.random() * acceptedReplies.length));
         const result = acceptedReplies[random];
 
@@ -11,19 +11,19 @@ const acceptedReplies = ['rock', 'paper', 'scissors'];
         if (!acceptedReplies.includes(choice)) return message.channel.send(`Only these responses are accepted: \`${acceptedReplies.join(', ')}\``);
         
         console.log('Bot Result:', result);
-        if (result === choice) return message.reply("It's a tie! We had the same choice.");
+        if (result === choice) return message.reply("Unentschieden.");
         
         switch (choice) {
-            case 'rock': {
-                if (result === 'paper') return message.reply('I won!');
+            case 'stein': {
+                if (result === 'papier') return message.reply('I won!');
                 else return message.reply('You won!');
             }
-            case 'paper': {
-                if (result === 'scissors') return message.reply('I won!');
+            case 'papier': {
+                if (result === 'schere') return message.reply('I won!');
                 else return message.reply('You won!');        
             }
-            case 'scissors': {
-                if (result === 'rock') return message.reply('I won!');
+            case 'schere': {
+                if (result === 'stein') return message.reply('I won!');
                 else return message.reply('You won!');
             }
             default: {

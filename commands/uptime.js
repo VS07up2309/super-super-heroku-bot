25e0602d76ff595df3ceb97d8f99836944e558b9
ImCoolAndYouUwU <65310408+VS07up2309@users.noreply.github.com>
2020-08-29 15:18:49 +0200

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -1,10 +1,9 @@
-module.export { 
+module.export = { 
   name: 'uptime',
   execute(message) {
   const moment = require("moment");
 require("moment-duration-format");
 const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
-
 message.channel.send(duration);
 }
 };

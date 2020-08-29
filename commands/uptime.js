dc096e138a1374185e6ac808005f17a247b640ae
ImCoolAndYouUwU <65310408+VS07up2309@users.noreply.github.com>
2020-08-29 15:12:01 +0200

    Create uptime.js

---
+++ commands/uptime.js
@@ -0,0 +1,10 @@
+module.export { 
+  name: 'uptime',
+  execute(message) {
+  const moment = require("moment");
+require("moment-duration-format");
+const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
+
+message.channel.send(duration);
+}
+};

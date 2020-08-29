b0a53f3c670b46e3ef455e806386fee3e0c8c897
ImCoolAndYouUwU <65310408+VS07up2309@users.noreply.github.com>
2020-08-29 18:58:30 +0200

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -9,6 +9,8 @@ module.exports = {
     message.reply('Keine Berechtigung zum Kicken...')
     return;
   }
+		
+		
 const user = message.mentions.users.first();
      // If we have a user mentioned
      if (user) {
@@ -25,23 +27,23 @@ const user = message.mentions.users.first();
            .kick('Optional reason that will display in the audit logs')
            .then(() => {
              // We let the message author know we were able to kick the person
-             message.reply(`Successfully kicked ${user.tag}`);
+             message.reply(`${user.tag} Wurde erfolgreich gekickt`);
            })
            .catch(err => {
              // An error happened
              // This is generally due to the bot not being able to kick the member,
              // either due to missing permissions or role hierarchy
-             message.reply('I was unable to kick the member');
+             message.reply('Fehler beim Kicken');
              // Log the error
              console.error(err);
            });
        } else {
          // The mentioned user isn't in this guild
-         message.reply("That user isn't in this guild!");
+         message.reply("Dieser Benutzer ist nicht in diesem Server!");
        }
        // Otherwise, if no user was mentioned
      } else {
-       message.reply("You didn't mention the user to kick!");
+       message.reply("Keinen Benutzer zum kicken markiert!");
      }
    }
  };

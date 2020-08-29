9ac0bde90eb4d19a450310184752fd255c657d90
ImCoolAndYouUwU <65310408+VS07up2309@users.noreply.github.com>
2020-08-29 18:24:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,76 +4,126 @@ const Discord = require('discord.js');
 var config = new Object();
 config.prefix = "!"
 const client = new Discord.Client();
-client.commands = new Discord.Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-const cooldowns = new Discord.Collection();
-
-for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-
-    // set a new item in the Collection
-    // with the key as the command name and the value as the exported module
-    client.commands.set(command.name, command);
+client.commands = new Discord
+	.Collection();
+const commandFiles = fs.readdirSync(
+	'./commands').filter(file =>
+	file.endsWith('.js'));
+const cooldowns = new Discord
+	.Collection();
+for (const file of commandFiles)
+{
+	const command = require(
+		`./commands/${file}`);
+	// set a new item in the Collection
+	// with the key as the command name and the value as the exported module
+	client.commands.set(command.name,
+		command);
 }
-
-client.on('ready', () => {
-    console.log(`Bot has started, with ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`);
-    client.user.setActivity(`Serving ${client.guilds.cache.size} servers`);
-
+client.on('ready', () =>
+{
+	console.log(
+		`Bot has started, with ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`
+	);
+	client.user.setActivity(
+		`Serving ${client.guilds.cache.size} servers`
+	);
 });
-
-client.on('message', message => {
-    if (!message.content.startsWith(config.prefix) || message.author.bot) return;
-
-    const args = message.content.slice(config.prefix.length).trim().split(/ +/);
-    const commandName = args.shift().toLowerCase();
-
-    const command = client.commands.get(commandName) ||
-        client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-
-    if (!command) return;
-
-    if (command.guildOnly && message.channel.type === 'dm') {
-        return message.reply('I can\'t execute that command inside DMs!');
-    }
-
-    if (command.args && !args.length) {
-        let reply = `You didn't provide any arguments, ${message.author}!`;
-
-        if (command.usage) {
-            reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
-        }
-
-        return message.channel.send(reply);
-    }
-
-    if (!cooldowns.has(command.name)) {
-        cooldowns.set(command.name, new Discord.Collection());
-    }
-
-    const now = Date.now();
-    const timestamps = cooldowns.get(command.name);
-    const cooldownAmount = (command.cooldown || 3) * 1000;
-
-    if (timestamps.has(message.author.id)) {
-        const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-
-        if (now < expirationTime) {
-            const timeLeft = (expirationTime - now) / 1000;
-            return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
-        }
-    }
-
-    timestamps.set(message.author.id, now);
-    setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
-
-    try {
-        command.execute(message, args);
-    } catch (error) {
-        console.error(error);
-        message.reply('there was an error trying to execute that command!');
-    }
+client.on('message', message =>
+{
+	if (!message.content
+		.startsWith(config
+			.prefix) || message
+		.author.bot) return;
+	const args = message.content
+		.slice(config.prefix
+			.length).trim()
+		.split(/ +/);
+	const commandName = args
+		.shift().toLowerCase();
+	const command = client
+		.commands.get(
+			commandName) ||
+		client.commands.find(
+			cmd => cmd
+			.aliases && cmd
+			.aliases.includes(
+				commandName));
+	if (!command) return;
+	if (command.guildOnly &&
+		message.channel.type ===
+		'dm')
+	{
+		return message.reply(
+			'I can\'t execute that command inside DMs!'
+		);
+	}
+	if (command.args && !args
+		.length)
+	{
+		let reply =
+			`You didn't provide any arguments, ${message.author}!`;
+		if (command.usage)
+		{
+			reply +=
+				`\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+		}
+		return message.channel
+			.send(reply);
+	}
+	if (!cooldowns.has(command
+			.name))
+	{
+		cooldowns.set(command
+			.name,
+			new Discord
+			.Collection());
+	}
+	const now = Date.now();
+	const timestamps = cooldowns
+		.get(command.name);
+	const cooldownAmount = (
+		command.cooldown ||
+		3) * 1000;
+	if (timestamps.has(message
+			.author.id))
+	{
+		const expirationTime =
+			timestamps.get(
+				message.author
+				.id) +
+			cooldownAmount;
+		if (now <
+			expirationTime)
+		{
+			const timeLeft = (
+				expirationTime -
+				now) / 1000;
+			return message
+				.reply(
+					`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`
+				);
+		}
+	}
+	timestamps.set(message
+		.author.id, now);
+	setTimeout(() => timestamps
+		.delete(message
+			.author.id),
+		cooldownAmount);
+	try
+	{
+		command.execute(message,
+			args);
+	}
+	catch (error)
+	{
+		console.error(error);
+		message.reply(
+			'there was an error trying to execute that command!'
+		);
+	}
 });
-
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.DISCORD_SECRET);
+client.login(process.env
+	.DISCORD_SECRET);

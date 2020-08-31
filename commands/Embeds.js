3f64ad750d6da56b61f172910da1d772f01e4e66
ImCoolAndYouUwU <65310408+VS07up2309@users.noreply.github.com>
2020-08-31 21:10:17 +0200

    Update Embeds.js

--- commands/Embeds.js
+++ commands/Embeds.js
@@ -1,10 +1,10 @@
 module.exports = {
   name: 'embed',
   execute(message) {
-    const test = new Discord.MessageEmbed()
-          .setColor('#00FDFF')
-          .setTitle('Test')
-          .setDescription('Lmaooooo')
+    const test = new Discord.MessageEmbed();
+          test.setColor = ('#00FDFF');
+          test.setTitle = ('Test');
+          test.setDescription = ('Lmaooooo');
   }
   
   channel.send(test);

fc4ada54f85991d7dcc8871bf35597891a0cc1a5
ImCoolAndYouUwU <65310408+VS07up2309@users.noreply.github.com>
2020-08-31 19:29:55 +0200

    Create getuuid.js

---
+++ commands/getuuid.js
@@ -0,0 +1,16 @@
+module.exports = {
+    name: 'getuuid',
+    description: 'get uuid by name',
+    args: true,
+    execute(message, args) {
+        const MinecraftAPI = require('minecraft-api');
+    
+    if (args[0] === 'foo') {
+			return message.channel.send('bar');
+		}
+    
+        function getuuid() {
+            MinecraftAPI.uuidForName(args)
+              .then(uuid => console.log(uuid))
+                .catch(err => console.log(err))
+        }

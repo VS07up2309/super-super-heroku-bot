e8d042f2786dc28a5f297e92bbcd0ed96c03c9a1
ImCoolAndYouUwU <65310408+VS07up2309@users.noreply.github.com>
2020-08-31 21:01:36 +0200

    Create Embeds.js

---
+++ commands/Embeds.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'embed',
+  execute(message) {
+    const test = new Discord.MessageEmbed()
+          .setColor('#00FDFF')
+          .setTitle('Test')
+          .setDescription('Lmaooooo');
+  }
+ };

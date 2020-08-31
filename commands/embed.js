0094285595bda8a697acd36b283d7e40af8d4da8
ImCoolAndYouUwU <65310408+VS07up2309@users.noreply.github.com>
2020-08-31 21:15:00 +0200

    Create embed.js

---
+++ commands/embed.js
@@ -0,0 +1,11 @@
+module.exports = {
+   name: 'embed',
+   execute(message) {
+     const test = new Discord.MessageEmbed();
+           test.setColor = ('#00FDFF');
+           test.setTitle = ('Test');
+           test.setDescription = ('Lmaooooo');
+   }
+
+   channel.send(test);
+  };

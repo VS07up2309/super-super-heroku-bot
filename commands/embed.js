6ba601a304dcb385cfb04a75c3401e4f9c7eb3a5
ImCoolAndYouUwU <65310408+VS07up2309@users.noreply.github.com>
2020-08-31 21:31:13 +0200

    Update embed.js

--- commands/embed.js
+++ commands/embed.js
@@ -3,35 +3,14 @@ module.exports = {
    execute(message) {
      const Discord = require("discord.js");
       
-      const embed = new Discord.RichEmbed()
-  .setTitle("This is your title, it can hold 256 characters")
-  .setAuthor("Author Name", "https://i.imgur.com/lm8s41J.png")
-  /*
-   * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
-   */
-   
-  .setColor(0x00AE86)
-  .setDescription("This is the main body of text, it can hold 2048 characters.")
-  .setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
-  .setImage("http://i.imgur.com/yVpymuV.png")
-  .setThumbnail("http://i.imgur.com/p2qNFag.png")
-  /*
-   * Takes a Date object, defaults to current date.
-   */
-  .setTimestamp()
-  .setURL("https://discord.js.org/#/docs/main/indev/class/RichEmbed")
-  .addField("This is a field title, it can hold 256 characters",
-    "This is a field value, it can hold 1024 characters.")
-  /*
-   * Inline fields may not display as inline if the thumbnail and/or image is too big.
-   */
-  .addField("Inline Field", "They can also be inline.", true)
-  /*
-   * Blank field, useful to create some space.
-   */
-  .addBlankField(true)
-  .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
- 
-  message.channel.send({embed});
+      var embed = new Discord.MessageEmbed()
+    .setAuthor(message.author.username)
+    .setDescription("Usuario rikolino.")
+    .setColor("#3535353")
+    .addField("Usuario", '${message.author.username}#${message.author.discriminator}')
+    .addField("ID", message.author.id)
+    .addField("Creación", message.author.createdAt);
+
+message.channel.send(embed);
    }
 };

b9fa3c2a5549c5ecc585800e1381c7bc8cf9a79f
ImCoolAndYouUwU <65310408+VS07up2309@users.noreply.github.com>
2020-09-01 20:59:38 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,6 @@
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database(':memory:');
+
 const fs = require('fs');
 const chalk = require('chalk');
 const Discord = require('discord.js');

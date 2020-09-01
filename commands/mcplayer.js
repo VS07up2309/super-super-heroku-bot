631e1480cae2a04ba86bf1e022d541afa4a4cbca
ImCoolAndYouUwU <65310408+VS07up2309@users.noreply.github.com>
2020-09-01 20:18:42 +0200

    Update mcplayer.js

--- commands/mcplayer.js
+++ commands/mcplayer.js
@@ -13,19 +13,11 @@ module.exports = {
 	  	mojangjs.getUUID(args).then(uuid => {
     			console.log(args);
 			console.log(uuid);
-			message.channel.send(uuid);
+			
 		}).catch(err => console.error(err));
 	  
 	  
-	  if (client.user.lastMessage == null) {
-                const collector = new Discord.MessageCollector(msg.channel, m => m.author.id === client.user.id, { time: 10000 });
-                collector.on('collect', message => {
-                    console.log(message.content);
-                    collector.stop("Got my message");
-                })
-            } else {
-                console.log(client.user.lastMessage.content);
-            };
+	  
 	  
 	  var player = new Discord.MessageEmbed()
 			.setThumbnail('https://crafatar.com/avatars/' + (uuid) + '?size=100')

02331bcb7249a55e737591bd073d06cb0047ac0f
ImCoolAndYouUwU <65310408+VS07up2309@users.noreply.github.com>
2020-10-01 23:07:01 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,4 +1,5 @@
-const prefix = "!";
+const config = require('../config.json');
+
 module.exports = {
 	name: 'help',
 	description: 'List all of my commands or info about a specific command.',
@@ -14,7 +15,7 @@ const { commands } = message.client;
 if (!args.length) {
 data.push('Here\'s a list of all my commands:');
 data.push(commands.map(command => command.name).join(', '));
-data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
+data.push(`\nYou can send \`${config.prefix}help [command name]\` to get info on a specific command!`);
 
 return message.author.send(data, { split: true })
 	.then(() => {
@@ -40,7 +41,7 @@ return message.author.send(data, { split: true })
 
 		if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
 		if (command.description) data.push(`**Description:** ${command.description}`);
-		if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
+		if (command.usage) data.push(`**Usage:** ${config.prefix}${command.name} ${command.usage}`);
 
 		data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
 
